8b5cccd8537316efda52c63c41bc297bad3ced88
Quinner64 <quinneryyc@gmail.com>
2023-11-16 16:11:06 -0500

    Updated load_dataset function

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # CMPE/CISC 452 NLP Text Classification
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# Main function
-if __name__ == '__main__':
+#For Preprocessing
+import re    # RegEx for removing non-letter characters
+import nltk  #natural language processing
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+
+def load_datasets():
     # Load Tweet dataset
     df1 = pd.read_csv('dataset/Twitter_Data.csv')
 
@@ -26,10 +33,9 @@ if __name__ == '__main__':
     # Output first five rows
     print(df3.head())
 
-
     df = pd.concat([df1, df2, df3], ignore_index=True)
 
-    print(df.isnull().sum())
+    df.isnull().sum()
 
     # drop missing rows
     df.dropna(axis=0, inplace=True)
@@ -41,5 +47,51 @@ if __name__ == '__main__':
     df['category'] = df['category'].map({-1.0: 'Negative', 0.0: 'Neutral', 1.0: 'Positive'})
 
     # Output first five rows
-    print(df.head())
+    df.head()
+
+    # Return Data Frame
+    return df
+
+def data_preprocessing(tweet):
+    # convert to lowercase
+    text = tweet.lower()
+    # remove non letters
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    # tokenize
+    words = text.split()
+    # remove stopwords
+    words = [w for w in words if w not in stopwords.words("english")]
+    # apply stemming
+    words = [PorterStemmer().stem(w) for w in words]
+
+    # return list
+    return words
+
+
+
+# Main function
+if __name__ == '__main__':
+    df = load_datasets()
+    # The distribution of sentiments
+    df.groupby('category').count().plot(kind='bar')
+
+    # Grouping by 'category' and counting occurrences, then plotting the counts as a bar plot
+
+    plt.xlabel('Category')
+    plt.ylabel('Value')
+    plt.title('Tweet Data Visualization')
+    plt.show()
+
+    # Apply data processing to each tweet
+    X = list(map(data_preprocessing, df['clean_text']))
+
+    from sklearn.preprocessing import LabelEncoder
+
+    # Encode target labels
+    le = LabelEncoder()
+    Y = le.fit_transform(df['category'])
+
+    print(X[0])
+    print(Y[0])
+
 
